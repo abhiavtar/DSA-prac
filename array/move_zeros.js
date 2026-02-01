f5d37268bd5326a39e3d111f95d127cf9e53d3ff
Abhijit Ghosh <abhijitghosh916.com>
2026-02-01 20:36:22 +0530

    Add max consecutive ones solution

---
+++ array/move_zeros.js
@@ -0,0 +1,15 @@
+var moveZeroes = function(nums) {
+    let x = 0;
+    for(let i=0; i < nums.length; i++){
+        if(nums[i] !==0){
+         nums[x]= nums[i];
+         x++
+        }
+      
+    }
+    // to fill the last item as zeros
+    for(let i = x; i< nums.length;i++){
+        nums[i]=0;
+    }
+    
+};
